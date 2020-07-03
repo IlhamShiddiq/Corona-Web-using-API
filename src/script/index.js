1edd00b6a14eac45105aa442e33690f9d4de75a0
IlhamShiddiq <shdqillham123@gmail.com>
2020-07-03 20:09:27 +0700

    bundle js, css, and image files with Webpack

--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,7 @@
+import "./../style/style.css";
+import "./../style/responsive.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./component/nav-bottom.js";
 import "./component/nav-top.js";
+import $ from "jquery";
+import 'bootstrap';
